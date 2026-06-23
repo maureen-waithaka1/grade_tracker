89abc6baaca3674f5974e3585df7ef0d5db87be7
Maureen Waithaka <waithakamaureenw@gmail.com>
2026-06-23 14:20:14 +0300

    updated models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemyimport Column, Integer, String, Float # Column is how we define a column in our table
+from database import Base # We import Base from own database.py file. Base is the parent class
+
+class Student(Base): # this create a python class called Student. By inheriting from base
+    __tablename__ = "students" # this sets the actual name of the table inside the database file
+
+    id = Column(Integer, primary_key=True, index=True)# id is a column in the table, is an integer
+    name = Column(String, nullable=False) # nullable = False - means this field is required
+    course = column(String, nullable=False)
+    grade = Column(Float, default=0.0) # grade is a column, ad the data type is float, default to 0.0
+    email = Column(String, unique=True)
+    
